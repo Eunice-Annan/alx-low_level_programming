621fca3ca3d48b96fc0a91d62148cb612191007c
Eunice-Annan <annaneunice4@gmail.com>
2022-09-09 09:17:15 -0700

    6. Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Print size of various typesof variables
+ *
+ *Return:Always 0(Success)
+ */
+int main(void)
+{
+  char c;
+int i;
+long int l;
+long long int o;
+float f;
+
+       printf("Size of a char: %d byte(s)\n", size of (c));
+       printf("Size of an int: %d byte(s)\n" size of (i));
+       printf("Size of a long int: %d byte(s)\n", size of (l));
+       printf("Size of a long long int: %d byte(s)\n", size of (o));
+       printf("Size of afloat: %d byte(s)\n", size of (f));
+       return(0)
+}
